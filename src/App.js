fccfb24396de91b1f1384886780fb91ffe7b5e0d
Edson Heyser <heyser1@yahoo.com.br>
2020-05-25 22:56:03 -0400

    added travis deploy config

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Estou aprendedo e mudei o codigo!!!
+          Coloquei na AWS!!!
         </p>
         <a
           className="App-link"
