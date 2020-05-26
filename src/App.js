62ab4acde6900dd20360458d964533ddb470f760
Edson Heyser <heyser1@yahoo.com.br>
2020-05-26 18:58:09 -0400

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coloquei na AWS!!!
+          Coloquei na AWS! e agora com feature - muito!!
         </p>
         <a
           className="App-link"
